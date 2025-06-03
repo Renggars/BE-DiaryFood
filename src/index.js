8f15010229844cdeeb28c96c00f6299855b79782
firazhafiz <firazfulvianhafiz05@gmail.com>
2025-06-04 04:39:40 +0700

    tes

--- src/index.js
+++ src/index.js
@@ -3,37 +3,10 @@ import config from "./config/config.js";
 import logger from "./config/logger.js";
 import prisma from "../prisma/index.js";
 
-let server;
-
 if (prisma) {
   logger.info("Connected to Database");
-  server = app.listen(config.port, () => {
-    logger.info(`Listening to port http://localhost:${config.port}`);
-  });
+} else {
+  logger.error("Failed to connect to Database");
 }
 
-const exitHandler = () => {
-  if (server) {
-    server.close(() => {
-      logger.info("Server closed");
-      process.exit(1);
-    });
-  } else {
-    process.exit(1);
-  }
-};
-
-const unexpectedErrorHandler = (error) => {
-  logger.error(error);
-  exitHandler();
-};
-
-process.on("uncaughtException", unexpectedErrorHandler);
-process.on("unhandledRejection", unexpectedErrorHandler);
-
-process.on("SIGTERM", () => {
-  logger.info("SIGTERM received");
-  if (server) {
-    server.close();
-  }
-});
+export default app;
