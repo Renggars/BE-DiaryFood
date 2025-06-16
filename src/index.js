a41f788616c936128a4d35b46368148e280f3ef8
Ilham <mi1797128@gmail.com>
2025-06-16 19:04:12 +0700

    add app.listen

--- src/index.js
+++ src/index.js
@@ -7,12 +7,15 @@ const prisma = new PrismaClient();
 
 async function startServer() {
   try {
-    console.log(
-      "Attempting to connect to database with URL:",
-      config.database.url
-    );
+    console.log("Attempting to connect to database with URL:", config.database.url);
     await prisma.$connect();
     logger.info("Connected to Database");
+
+    const port = process.env.PORT || 4000;
+
+    app.listen(port, () => {
+      logger.info(`Server is running on http://localhost:${port}`);
+    });
   } catch (error) {
     logger.error("Failed to connect to Database:", error.message);
     app.use((req, res, next) => {
